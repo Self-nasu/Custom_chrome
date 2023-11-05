3ebdefbd01c731a26e0d5a8e31ec34a7c2a1a08a
Namanqw1 <nasuchacha1708@gmail.com>
2023-11-05 16:13:19 +0530

    1st test

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Handles your frontend UI logic.
+var mymenu = document.getElementById('navmenu')
+var own = document.getElementById('toggle-navmenu')
+var display = 0
+
+function navmenu()
+{
+    if(display == 1)
+    {
+        own.style.display = 'flex';
+        mymenu.style.display = 'none';
+        display = 0;
+    }
+    else
+    {
+        own.style.display = 'none';
+        mymenu.style.display = 'flex';
+        display = 1;
+    }
+}
